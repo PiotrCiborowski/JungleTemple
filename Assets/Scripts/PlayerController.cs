619945e48a80b664e97a59becc8a8339cf5a5e81
PiotrCiborowski <pcibor@gazeta.pl>
2021-08-31 01:12:20 +0200

    Started work on character falling + added couple sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,151 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     Animator animator;
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
+    AudioSource audioSource;
 
     bool isGrounded = true;
     bool isRunning = false;
     bool isCrouching = false;
     bool isForward = true;
 
+    float fallingMomentum;
+
     [SerializeField] Transform groundCheck;
     [SerializeField] float runSpeed = 2.5f;
     [SerializeField] float crouchSpeed = 1f;
+
+    [SerializeField] AudioClip[] footsteps;
+    [SerializeField] AudioClip fallHurt;
+    [SerializeField] AudioClip fallDeath;
+
+    [SerializeField] Image hurtFlash;
+    [SerializeField] float hurtTime = 0.05f;
+
+    public int heroHP = 3;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        Vector2 raycastTest = new Vector2(0, -0.25f);
-        Debug.DrawRay(groundCheck.position, raycastTest, Color.red);
+
+        Vector2 groundRaycast = new Vector2(0, -0.25f);
+
+        RaycastHit2D feet = Physics2D.Raycast(groundCheck.position, Vector2.down, Mathf.Abs(groundRaycast.y), 1<<8);
+
+        if (feet.collider == null)
+        {
+            isGrounded = false;
+            animator.SetBool("Falling", true);
+
+            //if ()
+
+            if (isCrouching)
+            {
+                isCrouching = false;
+                animator.Play("ToFalling");
+            }
+            else if (isRunning)
+            {
+                isRunning = false;
+                fallingMomentum = rb.velocity.x;
+                animator.Play("ToFalling");
+            }
+            else if (rb.gravityScale != 0 && animator.GetCurrentAnimatorStateInfo(0).IsName("StandingJump") || rb.gravityScale != 0 && animator.GetCurrentAnimatorStateInfo(0).IsName("RunningJump"))
+            {
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.6f)
+                    animator.Play("ToFalling");
+            }
+
+            rb.velocity = new Vector2(fallingMomentum, rb.velocity.y);
+        }
+        else if (LayerMask.LayerToName(feet.collider.gameObject.layer) == "Ground")
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Falling"))
+            {
+                if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 40)
+                {
+                    isGrounded = true;
+                    animator.SetBool("Falling", false);
+                    fallingMomentum = 0;
+
+                    Footstep();
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 40 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 50)
+                {
+                    heroHP -= 1;
+
+                    hurtFlash.enabled = true;
+                    Invoke("HurtFlash", hurtTime);
+
+                    if (heroHP <= 0)
+                    {
+                        isGrounded = true;
+                        spriteRenderer.flipX = false;
+                        animator.SetTrigger("Die");
+                        //animator.SetBool("Falling", false);
+                        audioSource.pitch = 1;
+                        audioSource.PlayOneShot(fallDeath);
+                        fallingMomentum = 0;
+                        CantMove();
+                    }
+                    else
+                    {
+                        isGrounded = true;
+                        animator.SetBool("Falling", false);
+                        audioSource.pitch = 1;
+                        audioSource.PlayOneShot(fallHurt);
+                        fallingMomentum = 0;
+                        CantMove();
+
+                        animator.SetBool("Crouch", true);
+                        Invoke("FallHurt", 2);
+                    }
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 50)
+                {
+                    heroHP = 0;
+
+                    hurtFlash.enabled = true;
+                    Invoke("HurtFlash", hurtTime);
+
+                    isGrounded = true;
+                    spriteRenderer.flipX = false;
+                    animator.SetTrigger("Die");
+                    //animator.SetBool("Falling", false);
+                    audioSource.pitch = 1;
+                    audioSource.PlayOneShot(fallDeath);
+                    fallingMomentum = 0;
+                    CantMove();
+                }
+            }
+        }
+
+        Debug.DrawRay(groundCheck.position, groundRaycast, Color.red);
+
+        if (Input.GetKeyDown("r"))
+        {
+            rb.transform.position = new Vector2(6, 0);          //DEBUG
+            animator.ResetTrigger("Die");
+            animator.Play("Standing");
+            CanMove();
+        }
 
         if (animator.GetBool("canMove"))                        //try to leave inputs here and move behaviour to fixedupdate
         {
-            if (Input.GetKey("right") && !Input.GetKey("left shift"))
+            if (Input.GetKey("right") && !Input.GetKey("left shift") && isGrounded)
             {
                 if (!isCrouching && !animator.GetCurrentAnimatorStateInfo(0).IsName("CrouchMove") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Crouch"))
                 {
@@ -78,7 +192,7 @@ public class PlayerController : MonoBehaviour
                     }
                 }
             }
-            else if (Input.GetKey("left") && !Input.GetKey("left shift"))
+            else if (Input.GetKey("left") && !Input.GetKey("left shift") && isGrounded)
             {
                 if (!isCrouching && !animator.GetCurrentAnimatorStateInfo(0).IsName("CrouchMove") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Crouch"))
                 {
@@ -128,12 +242,12 @@ public class PlayerController : MonoBehaviour
             {
                 animator.SetTrigger("Jump");
             }
-            else if (Input.GetKey("down") && !isRunning)
+            else if (Input.GetKey("down") && isGrounded && !isRunning)
             {
                 animator.SetBool("Crouch", true);
                 isCrouching = true;
             }
-            else if (Input.GetKey("left shift") && !isRunning)
+            else if (Input.GetKey("left shift") && isGrounded && !isRunning)
             {
                 if (Input.GetKeyDown("right"))
                 {
@@ -158,17 +272,25 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
+                animator.SetBool("Running", false);
+                animator.SetBool("Crouch", false);
+                animator.ResetTrigger("Step");
+                animator.ResetTrigger("Jump");
+                isRunning = false;
+                isCrouching = false;
+
                 if (isGrounded)
-                {
-                    animator.SetBool("Running", false);
-                    animator.SetBool("Crouch", false);
-                    animator.ResetTrigger("Step");
-                    animator.ResetTrigger("Jump");
-                    isRunning = false;
-                    isCrouching = false;
-                }
+                    CanMove();
             }
         }
+    }
+
+    void Footstep()
+    {
+        audioSource.clip = footsteps[Random.Range(0, footsteps.Length)];
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+
+        audioSource.PlayOneShot(audioSource.clip);
     }
 
     void ChangeForward()
@@ -178,14 +300,30 @@ public class PlayerController : MonoBehaviour
         spriteRenderer.flipX = !spriteRenderer.flipX;
     }
 
-    void ChangeMove()
+    void CantMove()
     {
-        animator.SetBool("canMove", !animator.GetBool("canMove"));
+        animator.SetBool("canMove", false);
+    }
+
+    void CanMove()
+    {
+        animator.SetBool("canMove", true);
     }
 
     void StopMove()
     {
         rb.velocity = new Vector2(0.5f, rb.velocity.y);
+    }
+
+    void HurtFlash()
+    {
+        hurtFlash.enabled = false;
+    }
+
+    void FallHurt()
+    {
+        animator.SetBool("Crouch", false);
+        CanMove();
     }
 
     void CrouchGetUpMove()
@@ -214,7 +352,10 @@ public class PlayerController : MonoBehaviour
 
     void JumpUpMove()
     {
-        rb.velocity = new Vector2(0.075f, 2.5f);
+        if (isForward)
+            rb.velocity = new Vector2(0.075f, 2.5f);
+        else if (!isForward)
+            rb.velocity = new Vector2(-0.075f, 2.5f);
     }
 
     void JumpUpCheckForward()
